868b9a58965aeedf0ae3fa874f05012689c72f28
ALPAL <allengriffin1009@gmail.com>
2020-11-21 10:04:46 -0500

    mapping of JobPositionPojo class with Hibernate annotations

---
+++ 11-8HDSproject/11-5HDSproject/src/main/java/com/hds/model/JobPositionPojo.java
@@ -0,0 +1,48 @@
+package com.hds.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "JobPosition")
+public class JobPositionPojo {
+
+
+    @Id
+    @Column(name = "JobPositionID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int job_position_id;
+
+    @Column(name = "JobTitle")
+    private int job_title;
+
+    @Column(name = "JobDescription")
+    private String job_description;
+
+
+    // Getter Setter methods
+
+
+    public int getJob_position_id() {
+        return job_position_id;
+    }
+
+    public void setJob_position_id(int job_position_id) {
+        this.job_position_id = job_position_id;
+    }
+
+    public int getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(int job_title) {
+        this.job_title = job_title;
+    }
+
+    public String getJob_description() {
+        return job_description;
+    }
+
+    public void setJob_description(String job_description) {
+        this.job_description = job_description;
+    }
+}
