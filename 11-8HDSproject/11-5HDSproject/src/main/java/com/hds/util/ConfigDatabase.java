c4f8da938a4acd2e9694119bf542e45e67c038a6
ALPAL <allengriffin1009@gmail.com>
2020-11-22 22:31:30 -0500

    The contact.html web page needed updating. Only the vertical nav bar. The last Category said "Employee Test" which is incorrect. I refactored it to Employee and added another category below for FAQs.
    ConfigDatabase class, I was testing code to see what would work. Only changes for this page is my "commented" out work.

--- 11-8HDSproject/11-5HDSproject/src/main/java/com/hds/util/ConfigDatabase.java
+++ 11-8HDSproject/11-5HDSproject/src/main/java/com/hds/util/ConfigDatabase.java
@@ -1,14 +1,21 @@
 package com.hds.util;
 
+import com.hds.model.Address;
 import com.hds.model.Customer;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Set;
 
 public class ConfigDatabase
 {
+
+	private static SessionFactory factory;
 
 	public void addItem(Customer customerPojo)
 	{
@@ -84,6 +91,8 @@ public class ConfigDatabase
 
 	public List<Customer> viewDB()
 	{
+
+		// THIS IS GOOD CODE
 		Transaction transaction = null;
 		List <Customer> customerList = null;
 		try (Session session = HibernateUtil.getSessionFactory().openSession())
@@ -91,6 +100,7 @@ public class ConfigDatabase
 			session.beginTransaction();
 //			Query query = session.createQuery("FROM CustomerPojo");
 			Query query = session.createQuery("FROM Customer c inner join c.address");
+
 
 
 			customerList = query.getResultList();
@@ -106,5 +116,46 @@ public class ConfigDatabase
 			}
 			e.printStackTrace();
 		}return customerList;
+
+
+
+		// ****This is a "TEST" good code is above
+//		factory = HibernateUtil.getSessionFactory();
+//		Session session = factory.getCurrentSession();
+//		Transaction tx = null;
+//
+//		try {
+//			tx = session.beginTransaction();
+//
+//			String queryText = "select a, c from Address a, Customer c " +
+//					" where a.address_id = c.customer_id";
+//
+//			List rows = session.createQuery(queryText).list();
+//
+//			for (Object row: rows) {
+//				System.out.println(" ------- ");
+//				System.out.println("Address: " + row.getClass());
+//				System.out.println("Customer: " + row.getClass());
+//			}
+//
+//			session.getTransaction();
+//			tx.commit();
+//
+//
+//		} catch (HibernateException e) {
+//			if (tx!=null) tx.rollback();
+//			e.printStackTrace();
+//		}
+
+
+//		String hql = "FROM Customer c inner join c.address";
+//		Set query = session.createQuery(hql);
+
+
+
+
+
+
+
 	}
 }
