908845de97ba862e9d259dee48e59ddb2e025f32
Dallas <60236681+Dalllas-H@users.noreply.github.com>
2020-11-26 00:46:32 -0500

    update "Adding" Customer final added comments

--- 11-8HDSproject/11-5HDSproject/src/main/java/com/hds/model/CustomerPojo.java
+++ 11-8HDSproject/11-5HDSproject/src/main/java/com/hds/model/CustomerPojo.java
@@ -1,8 +1,9 @@
 package com.hds.model;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 //@Entity(name = "Customer")
